27f35785a357adea425212e5854144b49296c54e
Dominik Miedziński <dominik@mdzn.pl>
2020-01-09 09:13:22 +0100

    Add docs

--- src/map.rs
+++ src/map.rs
@@ -1,14 +1,37 @@
 use crate::tree::Tree;
 
+/// A map implemented with prefix tree.
 #[derive(Debug, Clone, Default)]
 pub struct PrefixTreeMap<K, V> {
     root: Option<Tree<K, V>>,
 }
 
 impl<K: Eq + Clone, V> PrefixTreeMap<K, V> {
+    /// Creates an empty `PrefixTreeMap`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use prefix_tree::PrefixTreeMap;
+    ///
+    /// let mut map: PrefixTreeMap<u8, i32> = PrefixTreeMap::new();
+    /// ```
     pub fn new() -> PrefixTreeMap<K, V> {
         PrefixTreeMap { root: None }
     }
+
+    /// Returns `true` if the map contains a value for the specifiec key.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use prefix_tree::PrefixTreeMap;
+    ///
+    /// let mut map: PrefixTreeMap<u8, i32> = PrefixTreeMap::new();
+    /// map.insert("foo", 1);
+    /// assert_eq!(map.contains_key("foo"), true);
+    /// assert_eq!(map.contains_key("bar"), false);
+    /// ```
     pub fn contains_key<Q>(&self, key: Q) -> bool
     where
         Q: AsRef<[K]>,
@@ -16,10 +39,34 @@ impl<K: Eq + Clone, V> PrefixTreeMap<K, V> {
         self.get(key).is_some()
     }
 
+    /// Clears the map, removing all key-value pairs.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use prefix_tree::PrefixTreeMap;
+    ///
+    /// let mut map: PrefixTreeMap<u8, i32> = PrefixTreeMap::new();
+    /// map.insert("foo", 1);
+    /// map.clear();
+    /// assert!(map.is_empty());
+    /// ```
     pub fn clear(&mut self) {
         self.root = None
     }
 
+    /// Returns a reference to the value corresponding to the key.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use prefix_tree::PrefixTreeMap;
+    ///
+    /// let mut map: PrefixTreeMap<u8, i32> = PrefixTreeMap::new();
+    /// map.insert("foo", 1);
+    /// assert_eq!(map.get("foo"), Some(&1));
+    /// assert_eq!(map.get("bar"), None);
+    /// ```
     pub fn get<Q>(&self, key: Q) -> Option<&V>
     where
         Q: AsRef<[K]>,
@@ -31,6 +78,20 @@ impl<K: Eq + Clone, V> PrefixTreeMap<K, V> {
             .flatten()
     }
 
+    /// Returns a mutable reference to the value corresponding to the key.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use prefix_tree::PrefixTreeMap;
+    ///
+    /// let mut map: PrefixTreeMap<u8, i32> = PrefixTreeMap::new();
+    /// map.insert("foo", 1);
+    /// if let Some(x) = map.get_mut("foo") {
+    ///     *x = 2;
+    /// }
+    /// assert_eq!(map.get("foo"), Some(&2));
+    /// ```
     pub fn get_mut<Q>(&mut self, key: Q) -> Option<&mut V>
     where
         Q: AsRef<[K]>,
@@ -42,6 +103,19 @@ impl<K: Eq + Clone, V> PrefixTreeMap<K, V> {
             .flatten()
     }
 
+    /// Inserts a key-value pair into the map.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use prefix_tree::PrefixTreeMap;
+    ///
+    /// let mut map: PrefixTreeMap<u8, i32> = PrefixTreeMap::new();
+    /// map.insert("a", 42);
+    /// assert_eq!(map.is_empty(), false);
+    /// map.insert("a", 5);
+    /// assert_eq!(map.get("a"), Some(&5));
+    /// ```
     pub fn insert<Q>(&mut self, key: Q, value: V)
     where
         Q: AsRef<[K]>,
@@ -52,6 +126,19 @@ impl<K: Eq + Clone, V> PrefixTreeMap<K, V> {
             self.root = Some(Tree::new(key.as_ref().to_vec(), value));
         }
     }
+
+    /// Returns `true` if the map contains no elements.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use prefix_tree::PrefixTreeMap;
+    ///
+    /// let mut map: PrefixTreeMap<u8, i32> = PrefixTreeMap::new();
+    /// assert_eq!(map.is_empty(), true);
+    /// map.insert("foo", 1);
+    /// assert_eq!(map.is_empty(), false);
+    /// ```
     pub fn is_empty(&self) -> bool {
         self.root.is_none()
     }
--- src/set.rs
+++ src/set.rs
@@ -1,21 +1,55 @@
 use map::PrefixTreeMap;
 
+/// A set implemented as a `PrefixTreeMap` where the value is `()`.
 #[derive(Debug, Default)]
 pub struct PrefixTreeSet<T> {
     map: PrefixTreeMap<T, ()>,
 }
 
 impl<T: Eq + Clone> PrefixTreeSet<T> {
+    /// Creates an empty `PrefixTreeSet`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use prefix_tree::PrefixTreeSet;
+    ///
+    /// let mut set: PrefixTreeSet<u8> = PrefixTreeSet::new();
+    /// ```
     pub fn new() -> PrefixTreeSet<T> {
         PrefixTreeSet {
             map: PrefixTreeMap::new(),
         }
     }
 
+    /// Clears the set, removing all key-value pairs.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use prefix_tree::PrefixTreeSet;
+    ///
+    /// let mut set: PrefixTreeSet<u8> = PrefixTreeSet::new();
+    /// set.insert("foo");
+    /// set.clear();
+    /// assert!(set.is_empty());
+    /// ```
     pub fn clear(&mut self) {
         self.map.clear()
     }
 
+    /// Returns `true` if the set contains a value.
+
+    /// # Examples
+    ///
+    /// ```
+    /// use prefix_tree::PrefixTreeSet;
+    ///
+    /// let mut set: PrefixTreeSet<u8> = PrefixTreeSet::new();
+    /// set.insert("1");
+    /// assert_eq!(set.contains("1"), true);
+    /// assert_eq!(set.contains("2"), false);
+    /// ```
     pub fn contains<Q>(&self, key: Q) -> bool
     where
         Q: AsRef<[T]>,
@@ -23,6 +57,18 @@ impl<T: Eq + Clone> PrefixTreeSet<T> {
         self.map.get(key).is_some()
     }
 
+    /// Adds a value to the set.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use prefix_tree::PrefixTreeSet;
+    ///
+    /// let mut set: PrefixTreeSet<u8> = PrefixTreeSet::new();
+    /// set.insert("1");
+    /// set.insert("1");
+    /// assert_eq!(set.contains("1"), true);
+    /// ```
     pub fn insert<Q>(&mut self, key: Q)
     where
         Q: AsRef<[T]>,
@@ -30,6 +76,18 @@ impl<T: Eq + Clone> PrefixTreeSet<T> {
         self.map.insert(key, ())
     }
 
+    /// Returns `true` if the set contains no elements.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use prefix_tree::PrefixTreeSet;
+    ///
+    /// let mut set: PrefixTreeSet<u8> = PrefixTreeSet::new();
+    /// assert_eq!(set.is_empty(), true);
+    /// set.insert("foo");
+    /// assert_eq!(set.is_empty(), false);
+    /// ```
     pub fn is_empty(&self) -> bool {
         self.map.is_empty()
     }
